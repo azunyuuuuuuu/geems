dfc035252371be460b07736ae5884291a9e2074c
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2022-06-12 21:58:07 +0200

    ➕ basic input shenanigans

--- Pages/TwoZeroFourEight.razor.cs
+++ Pages/TwoZeroFourEight.razor.cs
@@ -1,7 +1,21 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace geems.Pages;
 
 public partial class TwoZeroFourEight
 {
+    private int?[,] playfield = new int?[4, 4];
+
+    private void KeyDownEventHandler(KeyboardEventArgs e)
+        => Console.WriteLine($"{nameof(KeyDownEventHandler)}: {GetLogString(e)}");
+
+    private void KeyUpEventHandler(KeyboardEventArgs e)
+        => Console.WriteLine($"{nameof(KeyUpEventHandler)}: {GetLogString(e)}");
+        
+    private void KeyPressEventHandler(KeyboardEventArgs e)
+        => Console.WriteLine($"{nameof(KeyPressEventHandler)}: {GetLogString(e)}");
+
+    private static string GetLogString(KeyboardEventArgs e)
+        => $"Key: {e.Key}, Code: {e.Code}, Location: {e.Location}, Repeat: {e.Repeat}, CtrlKey: {e.CtrlKey}, ShiftKey: {e.ShiftKey}, AltKey: {e.AltKey}, MetaKey: {e.MetaKey}, Type: {e.Type}";
 }
